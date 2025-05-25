d8978b54c153bfc4846dae17b1834dcfb8c8e0fb
Jo-bella <105103832+Jo-bella@users.noreply.github.com>
2025-05-25 22:20:06 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,59 @@
+-- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT
+    bookings.id AS booking_id,
+    users.id AS user_id,
+    users.first_name,
+    users.last_name,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+
+-- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or booking is not linked to a user
+-- If your DBMS supports FULL OUTER JOIN:
+SELECT
+    users.id AS user_id,
+    users.first_name,
+    users.last_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+
+-- If using MySQL or others without FULL OUTER JOIN, use this UNION-based workaround:
+SELECT
+    users.id AS user_id,
+    users.first_name,
+    users.last_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+LEFT JOIN bookings ON users.id = bookings.user_id
+
+UNION
+
+SELECT
+    users.id AS user_id,
+    users.first_name,
+    users.last_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM bookings
+LEFT JOIN users ON users.id = bookings.user_id;
